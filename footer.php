6db737560d2044f70c2e0b699ee8292b1dae16c3
RomualdYT <romuald@gameurnews.fr>
2025-06-11 21:17:00 +0200

    oui

---
+++ footer.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package The_Words
+ */
+
+    /**
+     * the_words_single_related_post - 10
+     * the_words_subescribe - 20
+     * the_words_instagram - 30
+    **/
+    do_action('the_words_footer_content','the_words_subescribe');
+    ?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" itemscope itemtype="http://schema.org/WPFooter">
+
+        <?php
+        $ed_footer_social_icon = get_theme_mod('ed_footer_social_icon',1);
+        $footer_background_image = get_theme_mod('footer_background_image');
+        if( 
+        	is_active_sidebar('the-words-footer-1') || 
+            is_active_sidebar('the-words-footer-2') || 
+            is_active_sidebar('the-words-footer-3') || 
+        	$ed_footer_social_icon){
+            echo '<div class="top-footer" style="background-image:url('.esc_url( $footer_background_image ).')">';
+            echo '<div class="ta-container clearfix">';
+
+            if( has_nav_menu('the-words-footer-menu') ):
+
+            	echo '<div class="ta-footer-menu">';
+	            wp_nav_menu( array(
+					'theme_location' => 'the-words-footer-menu',
+					'menu_id'        => 'footer-menu',
+					'depth'			 => 1,
+				) );
+				echo '</div>';
+
+	        endif;
+
+			if( $ed_footer_social_icon ){
+				echo '<div class="ta-footer-social-icon">';
+				do_action('the_words_social_icon_action');
+				echo '</div>';
+			}
+
+            if( is_active_sidebar('the-words-footer-1') || is_active_sidebar('the-words-footer-2') || is_active_sidebar('the-words-footer-3') ){
+
+    			echo '<div class="footer-widget-area">';
+                for ($x = 0; $x <= 3; $x++) {
+                    if( is_active_sidebar('the-words-footer-'.$x) ){
+
+                        echo '<div id="ta-footer-widget-'.$x.'" class="ta-footer-widget">';
+                            dynamic_sidebar('the-words-footer-'.$x);
+                        echo '</div>';
+
+                    }
+                }
+            }
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+
+        } ?>
+
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php $ed_footer_go_top_button = get_theme_mod('ed_footer_go_top_button',1);
+    if( $ed_footer_go_top_button ){ ?>
+
+    <span id="ta-go-top" class="ta-off"><i class="fa fa-angle-up" aria-hidden="true"></i></span>
+
+<?php } ?>
+
+<div id="dynamic-css"></div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/customizer/customizer.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * The Words Theme Customizer
+ *
+ * @package The_Words
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function the_words_customize_register( $wp_customize ) {
+	
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	$wp_customize->get_section( 'title_tagline' )->panel = 'the_words_general_panel';
+	$wp_customize->get_section( 'header_image' )->panel = 'the_words_general_panel';
+	$wp_customize->get_section( 'background_image' )->panel = 'the_words_general_panel';
+	$wp_customize->get_section( 'colors' )->panel = 'the_words_general_panel';
+
+	$wp_customize->add_setting('enable_site_title',
+	    array(
+	        'default' => 1,
+	        'capability' => 'edit_theme_options',
+	        'sanitize_callback' => 'the_words_sanitize_checkbox',
+	    )
+	);
+
+	$wp_customize->add_control('enable_site_title',
+	    array(
+	        'label' => esc_html__('Display Site Title and Tagline', 'the-words'),
+	        'section' => 'title_tagline',
+	        'type' => 'checkbox',
+	    )
+	);
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'the_words_customize_partial_blogname',
+		) );
+
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'the_words_customize_partial_blogdescription',
+		) );
+	}
+
+	$wp_customize->add_panel( 'the_words_general_panel',
+		array(
+			'title'      => esc_html__( 'General Settings', 'the-words' ),
+			'priority'   => 5,
+			'capability' => 'edit_theme_options',
+		)
+	);
+
+	$wp_customize->add_panel(
+        'the_words_home_panel', 
+    	array(
+    		'priority'       => 5,
+        	'capability'     => 'edit_theme_options',
+        	'theme_supports' => '',
+        	'title'          => esc_html__( 'Front Page Settings', 'the-words' ),
+        ) 
+    );
+
+	$wp_customize->add_panel( 'theme_option_panel',
+		array(
+			'title'      => esc_html__( 'Theme Options', 'the-words' ),
+			'priority'   => 10,
+			'capability' => 'edit_theme_options',
+		)
+	);
+	
+	require get_template_directory() . '/inc/customizer/preloader.php';
+	require get_template_directory() . '/inc/customizer/social-icon.php';
+	require get_template_directory() . '/inc/customizer/header.php';
+	require get_template_directory() . '/inc/customizer/single.php';
+	require get_template_directory() . '/inc/customizer/footer.php';
+	require get_template_directory() . '/inc/customizer/layoyt.php';
+	require get_template_directory() . '/inc/customizer/front-page.php';
+	require get_template_directory() . '/inc/customizer/subescribe.php';
+	require get_template_directory() . '/inc/customizer/instagram.php';
+	
+	/**  The Words Pro Link **/
+    class The_Words_Link_Section extends WP_Customize_Section {
+
+        public $type = 'the-words-pro';
+
+        public $pro_text = '';
+
+        public $pro_url = '';
+
+        public function json() {
+            $json = parent::json();
+            $json['pro_text'] = $this->pro_text;
+            $json['pro_url']  = esc_url( $this->pro_url );
+            return $json;
+        }
+        protected function render_template() { ?>
+
+            <li id="custom-section-{{ data.id }}" class="custom-section control-section control-section-{{ data.type }} cannot-expand">
+                <h3 class="custom-section-title" style="background: #5ca595; margin: 0; padding: 20px 5px 8px 5px; color: #fff;">
+                    {{ data.title }}
+                    <# if ( data.pro_text && data.pro_url ) { #>
+                        <a style="margin-left: 10px; transform: translateY(-5px);" href="{{ data.pro_url }}" class="button button-custom center" target="_blank">{{ data.pro_text }}</a>
+                    <# } #>
+                </h3>
+            </li>
+        <?php }
+    }
+
+	/** Upgrade to The Words Pro **/
+
+	$wp_customize->register_section_type( 'The_Words_Link_Section' );
+
+	// Register sections.
+	$wp_customize->add_section(
+	    new The_Words_Link_Section(
+	        $wp_customize,
+	        'the-words-pro',
+	        array(
+	            'title'    => esc_html__( 'Upgrade to The Words Pro', 'the-words' ),
+	            'pro_text' => esc_html__( 'Go Pro','the-words' ),
+	            'pro_url'  => 'https://themesarray.com/wordpress_themes/the-words-pro',
+	            'priority' => 1,
+	        )
+	    )
+	);
+
+}
+add_action( 'customize_register', 'the_words_customize_register' );
+
+require get_template_directory() . '/inc/customizer/sanitize.php';
+
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function the_words_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function the_words_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function the_words_customize_preview_js() {
+	wp_enqueue_script( 'the-words-customizer', get_template_directory_uri() . '/assets/core/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'the_words_customize_preview_js' );
+
+/**
+ * Customizer Enqueue Upsale.
+ */
+
+if (!function_exists('the_words_customizer_upsale')) :
+
+    function the_words_customizer_upsale(){   
+        
+        wp_enqueue_script('the-words-upsale', get_template_directory_uri() . '/assets/js/upsale.js', array('jquery'), '', 1);
+    }
+
+endif;
+
+add_action('customize_controls_enqueue_scripts', 'the_words_customizer_upsale');
